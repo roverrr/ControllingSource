e3db283421e3592fad17965cedb065ff595be606
Evgeny Smirnov <evgenysmirnov@MacBook-Pro-Evgeny.local>
2013-12-09 18:19:45 +0400

    Add log message on app launh Add button that logs then tapped

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -14,6 +14,12 @@
 
 @implementation ViewController
 
+#pragma mark - IBActions
+
+-(IBAction)clickTheButton:(id)sender
+{
+    NSLog(@"button pressed");
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
